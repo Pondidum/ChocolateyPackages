8ee14460a9a62dd31fa2e9109b93469cb035017c
andy <andy@andydote.co.uk>
2017-05-07 19:41:08 +0300

    initial version

---
+++ visual-studio-2017-professional/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'visual-studio-2017-professional'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://aka.ms/vs/15/release/vs_professional.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'visual-studio-2017-professiona'
+
+  checksum      = '85B5D6F3F038DD922A4FD0856122A2D0BD769893BBCC2A25B57834EE66C8810F'
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs = "--quiet --norestart --wait --add Microsoft.VisualStudio.Component.CoreEditor --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb"
+  validExitCodes = @(0, 3010)
+}
+
+Install-ChocolateyPackage @packageArgs
